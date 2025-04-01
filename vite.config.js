5d576254513619865c814d87d636cbcf9e528fb0
elfovo <152432815+elfovo@users.noreply.github.com>
2025-04-01 14:50:20 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/alv-immobilier/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+    sourcemap: true
+  }
 }) 

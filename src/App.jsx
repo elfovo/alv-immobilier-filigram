f11b40f1e648b301418beacb43292aad789fdddb
elfovo <152432815+elfovo@users.noreply.github.com>
2025-04-01 16:27:23 +0800

    Test de déploiement - modification du texte

--- src/App.jsx
+++ src/App.jsx
@@ -228,7 +228,7 @@ function App() {
               <img src="/alv-immobilier-filigram/Vitejs-logo.svg.png" alt="Vite Logo" className="tech-logo vite" />
               <img src="/alv-immobilier-filigram/React-icon.svg.png" alt="React Logo" className="tech-logo react" />
             </div>
-            <p>Glissez et déposez vos images ici</p>
+            <p>test</p>
             <p>ou cliquez pour sélectionner</p>
           </div>
         </div>
